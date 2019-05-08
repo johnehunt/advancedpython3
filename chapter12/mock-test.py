96b6b9629eec86dad9a963b77c6fcdbbf7416104
John Hunt <john@matraining.com>
2019-05-08 14:23:09 +0100

    Added test usiung mocking

--- chapter12/mock-test.py
+++ chapter12/mock-test.py
@@ -1,23 +1,28 @@
 import external_module
 
 from unittest.mock import *
+
 from unittest import TestCase
 from unittest import main
 import json
 
 
 def some_func():
+    # Calls out to external API - whcih we want to mock
     response = external_module.api_call()
     return response
 
 
-class test_api(TestCase):
+class test_some_func_calling_api(TestCase):
 
     @patch('external_module.api_call')
     def test_some_func(self, mock_api_call):
-        mock_api_call.return_value = MagicMock(status_code=200, response=json.dumps({'key':'value'}))
+        # Sets up mock version of api_call
+        mock_api_call.return_value = MagicMock(status_code=200, response=json.dumps({'key': 'value'}))
+        # Calls some_func(0 that calls the (mock) api_call() function
         result = some_func()
-        print(result.response)
+        print('result.response:', result.response)
+        # Check that the result returned from some_func() is what was expected
         self.assertEqual(result.status_code, 200, "returned status code is not 200")
         self.assertEquals(result.response, '{"key": "value"}', "response JSON incorrect")
 
