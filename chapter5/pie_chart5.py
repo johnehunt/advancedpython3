63afda1942b54dd11493989a01aec6ea032f120a
John Hunt <john@matraining.com>
2019-05-13 16:48:19 +0100

    Tidied up the layout

--- chapter5/pie_chart5.py
+++ chapter5/pie_chart5.py
@@ -2,14 +2,14 @@
 
 import matplotlib.pyplot as pyplot
 
-labels = 'Java', 'Scala', 'Python', 'C#'
-sizes = [30, 15, 45, 10]
+labels = 'Python', 'Scala', 'C#', 'Java'
+sizes = [45, 10, 15, 30]
 
 # only "explode" the 3rd slice (i.e. 'Python')
-explode = (0, 0, 0.1, 0)
+explode = (0.1, 0, 0, 0)
 
 fig1, ax1 = pyplot.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.f%%', shadow=True, startangle=270)
 
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
