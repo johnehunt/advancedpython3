b712c6dcad9c889fe20a6ea401777d5f115bff1a
John Hunt <john@matraining.com>
2019-05-13 16:12:18 +0100

    Added an example with a subplot and a figure

---
+++ chapter5/figure_example6.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as pyplot
+import numpy as np
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+# Initialize a Figure
+figure = pyplot.figure()
+
+# Add Axes to the Figure
+axis = figure.add_subplot(111)
+axis.plot(t, s, color='green', label='yyy')
+axis.set(xlabel='time (s)', ylabel='voltage (mV)', title='Simple Plot')
+
+# Show the grid
+axis.grid(linestyle='dashed')
+
+# Display the Legend
+axis.legend()
+
+# Generate the plot
+pyplot.show()
+
