9a23337e2c8861f33808f87644acf64393048ed0
John Hunt <john@matraining.com>
2019-05-14 11:19:57 +0100

    Added a simple line chart

---
+++ chapter5/line_graph3.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as pyplot
+
+x = [0, 1, 2, 3, 4, 5, 6]
+y = [0, 2, 6, 14, 30, 43, 75]
+
+# Set the axes headings
+pyplot.ylabel('Speed', fontsize=12)
+pyplot.xlabel('Time', fontsize=12)
+
+# Set the title
+pyplot.title("Speed v Time")
+
+# Plot and display the graph
+# Using blue circles for markers ('bo')
+# and a solid line ('-')
+pyplot.plot(x, y, 'bo-')
+pyplot.show()
