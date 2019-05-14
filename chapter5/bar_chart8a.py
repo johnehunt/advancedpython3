43d63bea7e4d97e15127cf8207f4cfef6d7722be
John Hunt <john@matraining.com>
2019-05-14 13:35:36 +0100

    Changed to use pyplot instead of plt

--- chapter5/bar_chart8a.py
+++ chapter5/bar_chart8a.py
@@ -1,13 +1,13 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
 import numpy as np
 
 labels = ('Python', 'Scala', 'C#', 'Java', 'PHP')
 y_pos = np.arange(len(labels))
 sizes = [45, 10, 15, 30, 22]
 
-plt.bar(y_pos, sizes, align='center')
-plt.xticks(y_pos, labels)
-plt.ylabel('Usage')
-plt.xlabel('Programming Languages')
+pyplot.bar(y_pos, sizes, align='center')
+pyplot.xticks(y_pos, labels)
+pyplot.ylabel('Usage')
+pyplot.xlabel('Programming Languages')
 
-plt.show()
+pyplot.show()
