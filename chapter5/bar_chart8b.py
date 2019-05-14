9c916e31ba388b4407dd60ddc32736ead4d21532
John Hunt <john@matraining.com>
2019-05-14 13:35:19 +0100

    Changed to use pyplot API

--- chapter5/bar_chart8b.py
+++ chapter5/bar_chart8b.py
@@ -3,28 +3,22 @@ import matplotlib.pyplot as plt
 n_groups = 5
 bar_width = 0.35
 
-teama_test_results = (60, 75, 56, 62, 58)
-teamb_test_result = (55, 68, 80, 73, 55)
-
-fig, ax = plt.subplots()
+teama_results = (60, 75, 56, 62, 58)
+teamb_results = (55, 68, 80, 73, 55)
 
 index_teama = range(n_groups)
 index_teamb = [i + bar_width for i in index_teama]
+
 ticks = [i + bar_width / 2 for i in index_teama]
+tick_labels = ('Lab 1', 'Lab 2', 'Lab 3', 'Lab 4', 'Lab 5')
 
-opacity = 0.7
+plt.bar(index_teama, teama_results, bar_width, color='b', label='Team A')
+plt.bar(index_teamb, teamb_results, bar_width, color='g', label='Team B')
 
-ax.bar(index_teama, teama_test_results, bar_width, alpha=opacity, color='b', label='Team A')
-ax.bar(index_teamb, teamb_test_result, bar_width, alpha=opacity, color='g', label='Team B')
-
-ax.set_xlabel('Labs')
-ax.set_ylabel('Scores')
-
-ax.set_title('Scores by Lab')
-ax.set_xticks(ticks)
-ax.set_xticklabels(('Lab 1', 'Lab 2', 'Lab 3', 'Lab 4', 'Lab 5'))
-
-ax.legend()
-fig.tight_layout()
+plt.xlabel('Labs')
+plt.ylabel('Scores')
+plt.title('Scores by Lab')
+plt.xticks(ticks, tick_labels)
+plt.legend()
 
 plt.show()
