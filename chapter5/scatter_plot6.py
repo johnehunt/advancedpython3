2540d1ed4db19684f224a107d5d2624699c9f253
John Hunt <john@matraining.com>
2019-05-16 14:30:28 +0100

    Simplified the scatter chart example

--- chapter5/scatter_plot6.py
+++ chapter5/scatter_plot6.py
@@ -1,21 +1,23 @@
-import numpy as np
 import matplotlib.pyplot as pyplot
 
 # Create data
-MAX_RANGE = 50
-sailing = (0.3 * np.random.rand(MAX_RANGE), 0.3 * np.random.rand(MAX_RANGE))
-swimming = (0.4 + 0.3 * np.random.rand(MAX_RANGE), 0.4 * np.random.rand(MAX_RANGE))
-riding = (0.8 + 0.3 * np.random.rand(MAX_RANGE), 0.6 * np.random.rand(MAX_RANGE))
+riding = ((17, 18, 21, 22, 19, 21, 25, 22, 25, 24),
+          (3, 6, 3.5, 4, 5, 6.3, 4.5, 5, 4.5, 4))
+swimming = ((17, 18, 20, 19, 22, 21, 23, 19, 21, 24),
+            (8, 9, 7, 10, 7.5, 9, 8, 7, 8.5, 9))
+sailing = ((31, 28, 29, 36, 27, 32, 34, 35, 33, 39),
+           (4, 6.3, 6, 3, 5, 7.5, 2, 5, 7, 4))
 
-data = ((riding, 'red', 'riding'),
-        (swimming, 'green', 'swimming'),
-        (sailing, 'blue', 'sailing'))
+# Plot the data
+pyplot.scatter(x=riding[0], y=riding[1], c='red', label='riding')
+pyplot.scatter(x=swimming[0], y=swimming[1], c='green', label='swimming')
+pyplot.scatter(x=sailing[0], y=sailing[1], c='blue', label='sailing')
 
-# Create plot
-for coords, color, activity in data:
-    x, y = coords
-    pyplot.scatter(x, y, c=color, label=activity)
-
+# Configure graph
+pyplot.xlabel('Age')
+pyplot.ylabel('Hours')
 pyplot.title('Activities Scatter Graph')
 pyplot.legend()
+
+# Display the chart
 pyplot.show()
