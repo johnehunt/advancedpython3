b80d82dab67cbedc69108ce6340fe22fe4628200
John Hunt <john@matraining.com>
2019-05-22 18:48:38 +0100

    changed output format

--- chapter21/reactive4.py
+++ chapter21/reactive4.py
@@ -4,7 +4,7 @@ from rx import operators as op
 
 # Set up a source with a filter
 source1 = rx.from_list([2, 3, 5, 7])
-source2 = rx.from_list([10,11,12])
+source2 = rx.from_list([10, 11, 12])
 
 rx.merge(source1, source2)\
-    .subscribe(lambda v: print(v))
+    .subscribe(lambda v: print(v, end=','))
