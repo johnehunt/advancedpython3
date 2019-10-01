ae83b45cd7f9f69a1da587bbd3532a508f1e7eea
John Hunt <john@matraining.com>
2019-10-01 11:00:16 +0100

    Added examples using the latest ActiveMQ Artemis release

--- messaging/publisher.py
+++ messaging/publisher.py
@@ -1,13 +1,16 @@
 import time
 import stomp
 
+print('Starting Publisher')
 hosts = [('localhost', 61616)]
 conn = stomp.Connection(host_and_ports=hosts)
-
 conn.start()
+
+print('Connecting to Message broker')
 conn.connect('admin', 'admin', wait=True)
 
-conn.send(body='Hello World' + time.time(), destination='/queue/test')
+print('Publishing a message to /queue/test')
+conn.send(body='Hello World - ' + str(time.time()), destination='/queue/test')
 
 input('Press enter (publisher) to quit')
 
--- messaging/receiver.py
+++ messaging/receiver.py
@@ -9,13 +9,17 @@ class MyListener(stomp.ConnectionListener):
         print('received a message "%s"' % message)
 
 
+print('Starting Receiver')
 hosts = [('localhost', 61616)]
 conn = stomp.Connection(host_and_ports=hosts)
 conn.set_listener('', MyListener())
 conn.start()
+
+print('Connecting to Message Broker')
 conn.connect('admin', 'admin', wait=True)
 
+print('Subscribing to queue/test')
 conn.subscribe(destination='/queue/test', id=1, ack='auto')
 
-input('Press enter to quit')
+input('Press enter to quit (Receiver)\n')
 conn.disconnect()
