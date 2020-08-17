a9a496e0df0c94c5104196849ec896b712ed7b85
John Hunt <john@matraining.com>
2020-08-17 13:25:15 +0100

    Extended excel reading examples

--- chapter20/readExcelExample.py
+++ chapter20/readExcelExample.py
@@ -7,26 +7,40 @@ from openpyxl import load_workbook
 def main():
     print('Starting reading Excel file using openPyXL')
 
-    workbook = load_workbook(filename='sample.xlsx')
+    # data_only = True loads cached reuslt of equations
+    # data_only = False load equations (default)
+    workbook = load_workbook(filename='sample.xlsx', data_only=True)
     print(workbook.active)
     print(workbook.sheetnames)
     print(workbook.worksheets)
 
-    print('-' * 10)
+    # print('-' * 10)
     ws = workbook['my worksheet']
-    print(ws['A1'])
-    print(ws['A1'].value)
-    print(ws['A2'].value)
-    print(ws['A3'].value)
+    # print(ws['A1'].value)
+    # print(ws['B1'].value)
+
+    # print('-' * 10)
+    # for sheet in workbook:
+    #     print(sheet.title)
 
     print('-' * 10)
-    for sheet in workbook:
-        print(sheet.title)
+    # print(ws.max_column)
+    # print(ws.max_row)
 
-    print('-' * 10)
-    cell_range = ws['A1':'A3']
+    cell_range = ws[ws.calculate_dimension()]
     for cell in cell_range:
-        print(cell[0].value)
+        try:
+            print(cell[0].value, end=", ")
+            age = int(cell[1].value)
+            if age > 18:
+                print('You can drink')
+            else:
+                print('Underage')
+        except ValueError:
+            print('Error in age data')
+            print(cell)
+        print(cell[2].value, cell[3].value, cell[4].value)
+
     print('-' * 10)
 
     print('Finished reading Excel file using openPyXL')
