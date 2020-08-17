abe96ea7ac65634fb60822b872b818c284e25224
John Hunt <john@matraining.com>
2020-08-17 17:47:29 +0100

    Removed unformatted text

--- chapter5/matplotlib2.py
+++ chapter5/matplotlib2.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as pyplot
 import numpy as np
 
-# Data for plotting
+# Data for plotting
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2 * np.pi * t)
 
-# Initialize a Figure
+# Initialize a Figure
 figure = pyplot.figure()
 
-# Add Axes to the Figure
+# Add Axes to the Figure
 axis = figure.add_subplot(111)
 axis.plot(t, s, color='green', label='yyy')
 axis.set(xlabel='time (s)', ylabel='voltage (mV)', title='Simple Plot')
