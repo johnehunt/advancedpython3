3b06c15557bf0aef579cb1bf3c4abbdb6cbf7a34
John Hunt <john.hunt10@gmail.com>
2020-09-14 15:41:21 +0100

    Split two query examples

--- chapter24/query1.py
+++ chapter24/query1.py
@@ -1,18 +1,5 @@
 import pymysql
 
-
-class Student:
-    def __init__(self, id, name, surname, subject, email):
-        self.id = id
-        self.name = name
-        self.surname = surname
-        self.subject = subject
-        self.email = email
-
-    def __str__(self):
-        return 'Student[' + str(id) + '] ' + name + ' ' + surname + ' - ' + subject + ' ' + email
-
-
 # Open database connection
 connection = pymysql.connect('localhost', 'user', 'password', 'uni-database')
 
@@ -29,12 +16,6 @@ print('cursor.description', cursor.description)
 data = cursor.fetchall()
 for row in data:
     print('row:', row)
-
-# Convert data into Student objects
-for row in data:
-    student_id, name, surname, subject, email = row
-    student = Student(student_id, name, surname, subject, email)
-    print(student)
 
 # disconnect from server
 connection.close()
