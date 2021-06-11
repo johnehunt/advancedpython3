73d52d275087ed2939d435050e26da272b0619e2
John Hunt <john.hunt10@gmail.com>
2021-06-11 10:28:41 +0100

    Added comments explaining behaviour of graphql example

--- graphql/main.py
+++ graphql/main.py
@@ -1,11 +1,18 @@
-from flask import Flask, request, jsonify
+# Now import the Ariadne elements
 from ariadne import graphql_sync, make_executable_schema, gql, load_schema_from_path
+# import PLAYGROUND_HTML, this is a useful tool for writing,
+# testing and learning about our GraphQL API.
+# It provides a web application front end to allow you to
+# run graphQL queries against your sever.
 from ariadne.constants import PLAYGROUND_HTML
+from flask import Flask, request, jsonify
 
 # Holds the resolvers
 from model import query, mutation
 
-
+# Setups up the schema with type definitions for
+# GraphQL to process. gql validates the schema and
+# schema is then make executable
 type_defs = gql(load_schema_from_path("./schema.graphql"))
 schema = make_executable_schema(type_defs, query, mutation)
 
@@ -13,31 +20,44 @@ schema = make_executable_schema(type_defs, query, mutation)
 app = Flask(__name__)
 
 
+# Set up path mapping for simple hello world response
+# Useful for testing
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+	return 'Hello, World!'
 
 
+# Sets up the URL for the GraphiQL playground application
+# Used with GET HTTP request Method
 @app.route("/graphql", methods=["GET"])
 def graphql_playground():
-    """Serve GraphiQL playground"""
-    return PLAYGROUND_HTML, 200
+	"""Serve GraphiQL playground"""
+	return PLAYGROUND_HTML, 200
 
 
+# GraphQL (single) end point. It will be used
+# for all GraphQL queries and mutations
+# By convention is is also /graphql but it
+# uses the POST HTTP Request method.
 @app.route("/graphql", methods=["POST"])
 def graphql_server():
-    data = request.get_json()
+	# Get the JSON request submitted via POST
+	data = request.get_json()
 
-    success, result = graphql_sync(
-        schema,
-        data,
-        context_value=request,
-        debug=app.debug
-    )
+	# Pass request data along with schema and
+	# original request to graphql_sync to work out
+	# which resolver function to call
+	success, result = graphql_sync(
+			schema,
+			data,
+			context_value=request,
+			debug=app.debug
+	)
 
-    status_code = 200 if success else 400
-    return jsonify(result), status_code
+	# If all ok return result and status code
+	status_code = 200 if success else 400
+	return jsonify(result), status_code
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
--- graphql/model.py
+++ graphql/model.py
@@ -1,8 +1,14 @@
+# Import the Query and Mutation types from Ariadne
 from ariadne import MutationType
 from ariadne import QueryType
 
+query = QueryType()
+mutation = MutationType()
 
+
+# Define class used to hold data to be return
 class Book:
+	""" Simple representation of a book """
 	def __init__(self, isbn, title, author, category, price):
 		self.isbn = isbn
 		self.title = title
@@ -11,22 +17,24 @@ class Book:
 		self.price = price
 
 
-query = QueryType()
-mutation = MutationType()
-
-# Data held by server
+# Initial data held by server
 books = [Book('123', 'Python Naturally', 'John Smith', "TECHNICAL", 12.99)]
 
 
+# Map field hello in a query to this
+# resolver function - could also be a method on a class
 @query.field("hello")
 def resolve_hello(_, info):
 	return "Hi there"
 
-
+# To invoke this resolver use this query in the GraphiQL browser
 # query {
 #   hello
 # }
 
+
+# Map the books field of a query to this resolver function
+# It will return the current list of books
 @query.field("books")
 def resolve_books(_, info):
 	return books
@@ -41,6 +49,7 @@ def resolve_books(_, info):
 #   }
 # }
 
+# Handles a request for a particular book
 @query.field("book")
 def resolve_book(*_, isbn):
 	print(isbn)
@@ -56,6 +65,11 @@ def resolve_book(*_, isbn):
 #  }
 # }
 
+
+# Defines a mutation resolver. The function takes the data supplied
+# by the mutation request. The info parameter contains the context which
+# is supplied with the request is processed. The resolver creates a new book
+# which is added to the list of books and the new book is returned
 @mutation.field("addBook")
 def resolve_add_book(_, info, isbn, title, author, category, price):
 	newBook = Book(isbn, title, author, category, price)
